ee1b2b3199e7ef86567344aa70476dce2d9838d8
Myeisha Swain <zse0296@gmail.com>
2022-11-22 17:05:56 -0500

    finished and tested all routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,45 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
-
-// The `/api/tags` endpoint
+// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll(
+    {
+      include:[{model:Product, through:ProductTag}]
+    }
+  ).then(TagData => res.json(TagData))
+  .catch(err => res.json(err))
+  // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // find one category by its `id` value
+  Tag.findOne(
+    {
+      where:{id:req.params.id},
+      include:[{model:Product, through:ProductTag}]
+    }
+  ).then(TagData => res.json(TagData))
+  .catch(err => res.json(err))
+  // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  // create a new category
+  Tag.create(req.body).then(TagData => res.json(TagData))
+  .catch(err => res.json(err))
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // update a category by its `id` value
+  Tag.update(req.body,{where:{id:req.params.id}}).then(TagData => res.json(TagData))
+  .catch(err => res.json(err))
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete a category by its `id` value
+  Tag.destroy({where:{id:req.params.id}}).then(TagData => res.json(TagData))
+  .catch(err => res.json(err))
 });
 
 module.exports = router;

d3591114744d8b8aa6d3d33e75e9dc5e2b1cb69b
Myeisha Swain <zse0296@gmail.com>
2022-11-22 16:16:33 -0500

    set up product and category route

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    category_name:
+    {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,

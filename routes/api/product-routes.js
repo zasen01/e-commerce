49f71a7d0f498533e52066402243b917f4261f57
Myeisha Swain <zse0296@gmail.com>
2022-11-22 17:00:20 -0500

    created product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll(
+    {
+      include:[Category,{model:Tag, through:ProductTag}]
+    }
+  ).then(categoryData => res.json(categoryData))
+  .catch(err => res.json(err))
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne(
+    {
+      where:{id:req.params.id},
+      include:[Category,{model:Tag, through:ProductTag}]
+    }
+  ).then(categoryData => res.json(categoryData))
+  .catch(err => res.json(err))
   // be sure to include its associated Category and Tag data
 });
 
@@ -91,6 +104,8 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({where:{id:req.params.id}}).then(ProductData => res.json(ProductData))
+  .catch(err => res.json(err))
 });
 
 module.exports = router;
